4fdd3a68a8441983e054e9f8e4e3f4469f75bce9
Gabyp <gabriela.p.arreaza@gmail.com>
2022-09-07 21:24:20 -0300

    Ejercicio 2a-Guia 1

---
+++ Ejercicio 2a.py
@@ -0,0 +1,5 @@
+# Definir dos variables con diferentes cadenas de texto que se concatenen y finalmente se muestren por pantalla.
+
+car_model='Kavak'
+car_brand='Toyota'
+print (car_model+' '+car_brand)
