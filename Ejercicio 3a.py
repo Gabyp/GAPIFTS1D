e74a380d257b086ae742d34d19f816d3586eeebb
Gabyp <gabriela.p.arreaza@gmail.com>
2022-09-11 08:58:08 -0300

    Ejercicio 3, Guia 1

---
+++ Ejercicio 3a.py
@@ -0,0 +1,10 @@
+#Ejercicio 3 Guia
+#Solicitar que el usuario ingrese Nombre y DNI, guardarlo en distintas variables e imprimir por 
+#pantalla “El nombre del usuario es XXXXXX y su DNI es YYYYYY”.
+#nombre = "Pedro Suarez"
+#dni= "9087654"
+
+nombre = input ("Ingrese su nombre ")
+dni= input ("Ingrese su DNI ")
+print (f'El nombre del usuario es: {nombre}', )
+print (f'El DNI del usuario es {dni}')      
