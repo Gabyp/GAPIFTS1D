eaa37463f375490b29fdcfb0885d770206385f6f
Gabyp <gabriela.p.arreaza@gmail.com>
2022-09-18 18:02:16 -0300

    Ejercicio 1, Guia 3 Funciones

---
+++ Ejer1c.py
@@ -0,0 +1,11 @@
+#Realizar una función que se llame area_rectangulo() que devuelva el área del cuadrado a partir de su base y altura.
+
+
+def area_rectangulo(b,h):
+    area=b*h
+    return area
+b= int (input("Ingrese el valor de la base "))
+h= int (input("Ingrese el valor de la altura "))
+
+a=b*h 
+print (f'Resultado del area es: {a}')
